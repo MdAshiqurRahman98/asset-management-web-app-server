a8f61e7f42abad7a8c33412b5fe95e5e65c77c67
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-11-23 20:47:27 +0600

    stripe added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
 const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
