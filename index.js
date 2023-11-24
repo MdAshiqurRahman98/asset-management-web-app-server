39b54e8bb7f5856af9fb7b79af2f8b198f109cd9
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-11-24 21:44:55 +0600

    custom middlewares added

--- index.js
+++ index.js
@@ -16,8 +16,6 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x5y82lv.mongodb.net/?retryWrites=true&w=majority`;
 
 // MongoDB connection
@@ -28,6 +26,31 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+
+// Custom middlewares
+const logger = async (req, res, next) => {
+    console.log('called: ', req.hostname, req.originalUrl);
+    console.log('log: info', req.method, req.url);
+    next();
+}
+
+const verifyToken = async (req, res, next) => {
+    const token = req?.cookies?.token;
+    // console.log('Token in the middleware: ', token);
+
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' });
+        }
+
+        req.user = decoded;
+        next();
+    })
+}
 
 async function run() {
     try {
